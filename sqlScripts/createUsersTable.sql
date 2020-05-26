d1fc5c401ba03f294bffa6c57c18c2e9436797dd
Gal <galrosenthal@gmail.com>
2020-05-27 00:45:56 +0300

    Added some functions and it works whoohoo

--- sqlScripts/createUsersTable.sql
+++ sqlScripts/createUsersTable.sql
@@ -1,6 +1,18 @@
 CREATE TABLE [dbo].[users](
 	[user_id] [UNIQUEIDENTIFIER] PRIMARY KEY NOT NULL default NEWID(),
-	[username] [varchar](30) NOT NULL UNIQUE,
-	[password] [varchar](300) NOT NULL
+	[username] [varchar](8) NOT NULL UNIQUE,
+	[password] [varchar](300) NOT NULL,
+	[firstName] [varchar](300) NOT NULL,
+	[lastName] [varchar](300) NOT NULL,
+	[email] [varchar](320) NOT NULL,
+	[profilePicUrl] [varchar](1024) NOT NULL
 )
 
+
+/*
+ "first-name": "John",
+    "last-name": "Doe",
+    "password": "Mashu123!",
+    "email": "john.doe@gmail.com",
+    "profile-pic":
+ */
